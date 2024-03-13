e4228b3efcf01e659f9461f92754c2ea1804e7f9
AlbaNunezArranz <albanunezarranz@gmail.com>
2024-03-13 13:41:04 +0100

    Tercer commit
    Tercer commit con do while y ventanas

---
+++ Js/10_Do_While.js
@@ -0,0 +1,13 @@
+var years = 25;
+
+do {
+    console.log ("Distinto de 25 " + years);
+    years++;
+
+    if(years == 24) {
+        break;
+    }
+
+} while (years <= 25)
+
+
---
+++ Js/11_Ventanas.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// ALERTA
+// alert ("Esta es mi alerta");
+// alert ("Este es el texto");
+
+// CONFIRMACION
+// puede meterse dentro de una variable o dejarlo fuera
+var resultado = confirm ("¿Estas seguro de querer continuar?");
+// con esto se puede ver la respuesta
+console.log(resultado);
+
+//INGRESO DATOS
+// puede meterse dentro de una variable o dejarlo fuera
+var resultado2 = prompt ("¿Que edad tienes?", 18 /* Este valor es por defecto */);
+console.log(resultado2);
+console.log(typeof resultado2);

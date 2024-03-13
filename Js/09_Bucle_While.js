6802f5a779715304389595383a61fa2332bad31b
AlbaNunezArranz <albanunezarranz@gmail.com>
2024-03-13 13:10:28 +0100

    Segundo commit (while)

---
+++ Js/09_Bucle_While.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var animal = "panda";
+
+while (animal === "panda") {
+    console.log ("Es un " + animal);
+}
+
+////////////////////////////////////
+
+var anyo = 2024;
+
+while (animal <= 2030) {
+    console.log ("Estamos en el año " + anyo);
+
+    anyo++;
+}

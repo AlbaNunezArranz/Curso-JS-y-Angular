fc0154d14a0ad769be4c7377fc8830a471951674
Alba <162701259+AlbaNunezArranz@users.noreply.github.com>
2025-03-07 15:41:41 +0100

    Add files via upload

---
+++ Js/36_Ordenar_arrays.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var mascotas = ["Perro", "Gato", "Conejo", "Pez"];
+var raza = ["Husky", "Mestizo", "Malamute", "Golden", "Beegle"];
+var tamaño = ["Pequño", "Mediano", "Grande"];
+
+mascotas.sort(); // por efecto lo ordena alfabeticamente
+console.log(mascotas)
+
+mascotas.reverse(); // lo ordena del reves
+console.log(mascotas)
+
+
+
+
+
+
